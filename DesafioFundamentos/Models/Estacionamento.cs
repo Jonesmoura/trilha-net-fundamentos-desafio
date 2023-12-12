2328854359adfa228027d9076ec291c3efb8ed81
Jones Moura <jones-ms@live.com>
2023-12-12 20:10:37 -0300

    Entrega desafio

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,49 +14,55 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.Write("Digite a placa do veículo para estacionar: ");
+            string placa = Console.ReadLine();
+            while(!ValidacaoDeDados.ValidacaoDePlaca(placa))
+            {
+                Console.Write("Formato de placa inválido, favor inserir a placa no Formato AAA-0000: ");
+                placa = Console.ReadLine();
+            }
+            veiculos.Add(placa);
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+            Console.Write("Digite a placa do veículo para remover: ");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
             string placa = "";
+            placa = Console.ReadLine();
+            while(!ValidacaoDeDados.ValidacaoDePlaca(placa)){
+                Console.Write("Formato de placa inválido, favor inserir a placa no Formato AAA-0000: ");
+                placa = Console.ReadLine();
+            }
 
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
+                Console.Write("Digite a quantidade de horas que o veículo ficou estacionado: ");
+                bool horasValidas = int.TryParse(Console.ReadLine(), out int horas);
+                while (!horasValidas || horas < 0)
+                {
+                    Console.WriteLine("Valor inválido, favor informar um número inteiro maior que 0: ");
+                    horasValidas = int.TryParse(Console.ReadLine(), out horas);
+                }
+                decimal valorTotal = precoInicial + precoPorHora * horas;
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa);
             }
             else
             {
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
             }
         }
-
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
             }
             else
             {
---
+++ DesafioFundamentos/Models/ValidacaoDeDados.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+    public class ValidacaoDeDados
+    {
+        public static bool ValidacaoDePlaca(string placa)
+        {
+            string padraoRegex = @"^[A-Za-z]{3}-\d{4}$";
+            return Regex.IsMatch(placa, padraoRegex);
+        }
+    }
+}
